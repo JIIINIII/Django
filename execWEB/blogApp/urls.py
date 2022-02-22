af29082b3d035337a186014f97a9739c68bd0c49
JINI <eldoljini@ajou.ac.kr>
2022-02-22 11:38:43 +0900

    WEB

---
+++ execWEB/blogApp/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path,include
+from blogApp import views
+
+urlpatterns = [
+    # http://127.0.0.1:8000/blog/index
+    path('index/',views.index),
+]
